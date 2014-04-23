492f485dda0b46af1fa6bb0ca243bbcf00b0adea
Sachith Withana <swsachith@gmail.com>
2014-04-23 11:11:46 -0400

    jaas configuration set programatically

--- src/main/java/lk/sachith/kerberos/HadoopConfiguration.java
+++ src/main/java/lk/sachith/kerberos/HadoopConfiguration.java
@@ -11,32 +11,36 @@ import java.util.Map;
 
 public class HadoopConfiguration
         extends javax.security.auth.login.Configuration {
-      private static final Map<String, String> BASIC_JAAS_OPTIONS =
-            new HashMap<String,String>();
+    private static final Map<String, String> BASIC_JAAS_OPTIONS =
+            new HashMap<String, String>();
 
-    private static final Map<String,String> USER_KERBEROS_OPTIONS =
-            new HashMap<String,String>();
+    private static final Map<String, String> USER_KERBEROS_OPTIONS =
+            new HashMap<String, String>();
+
     static {
         if (false) {
             USER_KERBEROS_OPTIONS.put("useDefaultCache", "true");
         } else {
             USER_KERBEROS_OPTIONS.put("doNotPrompt", "true");
             USER_KERBEROS_OPTIONS.put("useTicketCache", "true");
+            USER_KERBEROS_OPTIONS.put("debug", "true");
         }
-        String ticketCache = "/tmp/krb5cc_1005";
+        String ticketCache = "/Users/swithana/krb5cc_apache_swithana";
 //        String ticketCache = System.getenv("KRB5CCNAME");
         USER_KERBEROS_OPTIONS.put("ticketCache", ticketCache);
         USER_KERBEROS_OPTIONS.put("renewTGT", "true");
-        USER_KERBEROS_OPTIONS.putAll(BASIC_JAAS_OPTIONS);
+//        USER_KERBEROS_OPTIONS.putAll(BASIC_JAAS_OPTIONS);
     }
+
     private static final AppConfigurationEntry USER_KERBEROS_LOGIN =
-            new AppConfigurationEntry(KerberosUtil.getKrb5LoginModuleName(),
-                    AppConfigurationEntry.LoginModuleControlFlag.OPTIONAL,
+            new AppConfigurationEntry("com.sun.security.auth.module.Krb5LoginModule",
+                    AppConfigurationEntry.LoginModuleControlFlag.REQUIRED,
                     USER_KERBEROS_OPTIONS);
-    private static final Map<String,String> KEYTAB_KERBEROS_OPTIONS =
-            new HashMap<String,String>();
+    private static final Map<String, String> KEYTAB_KERBEROS_OPTIONS =
+            new HashMap<String, String>();
+
     static {
-        if (IBM_JAVA) {
+        if (false) {
             KEYTAB_KERBEROS_OPTIONS.put("credsType", "both");
         } else {
             KEYTAB_KERBEROS_OPTIONS.put("doNotPrompt", "true");
@@ -46,11 +50,18 @@ public class HadoopConfiguration
         KEYTAB_KERBEROS_OPTIONS.put("refreshKrb5Config", "true");
         KEYTAB_KERBEROS_OPTIONS.putAll(BASIC_JAAS_OPTIONS);
     }
-    private static final AppConfigurationEntry KEYTAB_KERBEROS_LOGIN =
+
+    private static final AppConfigurationEntry[] SIMPLE_CONF =
+            new AppConfigurationEntry[]{USER_KERBEROS_LOGIN};
+
+    @Override
+    public AppConfigurationEntry[] getAppConfigurationEntry(String name) {
+        return SIMPLE_CONF;
+    }
+    /*private static final AppConfigurationEntry KEYTAB_KERBEROS_LOGIN =
             new AppConfigurationEntry(KerberosUtil.getKrb5LoginModuleName(),
                     AppConfigurationEntry.LoginModuleControlFlag.REQUIRED,
-                    KEYTAB_KERBEROS_OPTIONS);
-
+                    KEYTAB_KERBEROS_OPTIONS);*/
 
 
 }
